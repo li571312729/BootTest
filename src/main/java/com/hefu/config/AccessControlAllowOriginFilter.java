74a456134aba4addd893eaac4a3d8296317c307a
xiaoqiangli <xiaoqiangli@trip.com>
2022-02-17 19:34:46 +0800

    跨域注释

--- src/main/java/com/hefu/config/AccessControlAllowOriginFilter.java
+++ src/main/java/com/hefu/config/AccessControlAllowOriginFilter.java
@@ -13,14 +13,21 @@ public class AccessControlAllowOriginFilter implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                // 允许任意源请求
                 .allowedOrigins("*")
+                // 允许的请求方法列表
                 .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
+                // 允许发送的请求头字段
                 .allowedHeaders("*")
+                // 允许前端通过XMLHttpRequest.getResponseHeader('X')  来获取 X（即以下）这些字段
                 .exposedHeaders("access-control-allow-headers",
                         "access-control-allow-methods",
                         "access-control-allow-origin",
                         "access-control-max-age",
                         "X-Frame-Options")
-                .allowCredentials(true).maxAge(3600);
+                // 是否允许客户端发送cookie
+                .allowCredentials(true)
+                // 预检请求的有效期，在此期间，不用发出另外一条预检请求。(非简单请求--option预检请求)
+                .maxAge(3600);
     }
 }
